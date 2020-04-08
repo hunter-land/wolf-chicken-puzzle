34ad20e8f23c9618075bd7a6f2e2de9fe8946090
landh <landh@oregonstate.edu>
2020-04-08 11:53:23 -0700

    Added a struct for states and argument parsing

--- main.cpp
+++ main.cpp
@@ -9,6 +9,45 @@
  * Hunter Land
  */
 
-int main(int argc, char *argv[]) {
+#include <iostream>
+#include <fstream>
+#include <string>
 
+struct state{
+	int wolvesOnLeft = 0;
+	int wolvesOnRight = 0;
+	int chickensOnLeft = 0;
+	int chickensOnRight = 0;
+	int boatsOnLeft = 0;
+	int boatsOnRight = 0;
+
+	// std::string toString() {
+	// 	return	"" + wolvesOnLeft + "," + chickensOnLeft + "," << boatsOnLeft << std::endl 
+	// 			<< wolvesOnRight << "," << chickensOnRight << "," << boatsOnRight << std::endl;
+	// }
+};
+
+std::ostream& operator<<(std::ostream& outs, const state s) {
+	outs << s.wolvesOnLeft << "," << s.chickensOnLeft << "," << s.boatsOnLeft << std::endl
+		 << s.wolvesOnRight << "," << s.chickensOnRight << "," << s.boatsOnRight << std::endl;
+}
+
+int main(int argc, char *argv[]) {
+	if(argc != 5) {
+		std::cerr << "This program requires 4 arguments:" << std::endl;
+		std::cerr << "\t<Initial State File>" << std::endl;
+		std::cerr << "\t<Goal State File>" << std::endl;
+		std::cerr << "\t<Mode>" << std::endl;
+		std::cerr << "\t<Output File>" << std::endl;
+		return 1;
+	}
+
+	std::string initStateFile(argv[1]);
+	std::string goalStateFile(argv[2]);
+	std::string modeString(argv[3]);
+	std::string outputFile(argv[4]);
+
+	//Do code
+
+	return 0;
 }
