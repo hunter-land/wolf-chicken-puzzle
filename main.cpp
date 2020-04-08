70584cb2c56848adee09261b2a39236dc6ab617b
landh <landh@oregonstate.edu>
2020-04-08 11:04:53 -0700

    Initial

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/**
+ * Solves the problem of getting a number of chickens and wolves across a river with a boat.
+ * The boat can only hold two animals and must hold at least one animal to move.
+ * If any side has more wolves than chickens, the chickens are eaten by the wolves and the problem is failed.
+ * 
+ * 
+ * 
+ * Aaron Koffel
+ * Hunter Land
+ */
+
+int main(int argc, char *argv[]) {
+
+}
